502293196e4378444cc5e3c82f2b51bf5771d32a
CaioCesarMDS <caiocesarsts@gmail.com>
2024-02-10 17:32:47 -0300

    chore: add inter font from expo google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/app/**/*.{ts,tsx}",
-    "./src/components/**/*.{ts,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./src/app/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                heading: "Inter_600SemiBold",
+                subtitle: "Inter_500Medium,",
+                body: "Inter_400Regular,",
+                bold: "Inter_700Bold",
+            },
+        },
+    },
+    plugins: [],
+};
