e1e678b0d45c1764ce1e7e616f29bc695aa51186
CaioCesarMDS <caiocesarsts@gmail.com>
2024-02-11 11:52:55 -0300

    chore: update products informations

--- src/utils/data/products.ts
+++ src/utils/data/products.ts
@@ -1,140 +1,145 @@
 const MENU = [
-  {
-    title: "Lanche do dia",
-    data: [
-      {
-        id: "1",
-        title: "X-React",
-        price: 24.9,
-        description:
-          "Um hamburger tão bonito que me deu fome enquanto eu fazia esse layout para o projeto...",
-        cover: require("../../assets/products/cover/1.png"),
-        thumbnail: require("../../assets/products/thumbnail/1.png"),
-        ingredients: [
-          "Pão brioche;",
-          "2x carnes smash (blend da casa) de 80g;",
-          "Queijo cheddar;",
-          "Alface;",
-          "Tomate;",
-          "Picles;",
-          "Cebola;",
-          "Molho da casa;",
+    {
+        title: "Lanche do dia",
+        data: [
+            {
+                id: "1",
+                title: "Hambúrguer Clássico",
+                price: 24.9,
+                description:
+                    "Um hambúrguer suculento de carne angus grelhada, queijo cheddar derretido, alface fresca, tomate e maionese caseira, tudo isso dentro de um pão de hambúrguer delicioso.",
+                cover: require("../../assets/products/cover/1.png"),
+                thumbnail: require("../../assets/products/thumbnail/1.png"),
+                ingredients: [
+                    "Pão brioche",
+                    "2x carnes smash (angus) de 80g",
+                    "Queijo cheddar",
+                    "Alface",
+                    "Tomate",
+                    "Cebola",
+                    "Maionese caseira",
+                ],
+            },
         ],
-      },
-    ],
-  },
-  {
-    title: "Promoções",
-    data: [
-      {
-        id: "2",
-        title: "X-JavaScript",
-        price: 34.9,
-        description:
-          "Os ingredientes desse hambúrguer mudam toda semana, então você sempre terá uma surpresa...",
-        cover: require("../../assets/products/cover/2.png"),
-        thumbnail: require("../../assets/products/thumbnail/2.png"),
-        ingredients: [
-          "Pão brioche;",
-          "2x carnes smash (blend da casa) de 80g;",
-          "Queijo cheddar;",
-          "Alface;",
-          "Tomate;",
-          "Picles;",
-          "Cebola;",
-          "Molho da casa;",
+    },
+    {
+        title: "Promoções",
+        data: [
+            {
+                id: "2",
+                title: "Hambúrguer Picante",
+                price: 34.9,
+                description:
+                    "Para os amantes de um toque ardente, este hambúrguer vem com carne grelhada, queijo pepper jack derretido, jalapeños em conserva e um pouco de molho de pimenta chipotle.",
+                cover: require("../../assets/products/cover/2.png"),
+                thumbnail: require("../../assets/products/thumbnail/2.png"),
+                ingredients: [
+                    "Pão australiano",
+                    "1 carne smash (blend da casa) de 120g",
+                    "Queijo pepper",
+                    "Alface",
+                    "Tomate",
+                    "Jalapenõs em conserva",
+                    "Cebola",
+                    "Molho de pimenta",
+                ],
+            },
+            {
+                id: "3",
+                title: "Hambúrguer BBQ",
+                price: 32.7,
+                description:
+                    "Um clássico americano com carne grelhada, queijo derretido, bacon crocante e molho barbecue defumado.",
+                cover: require("../../assets/products/cover/3.png"),
+                thumbnail: require("../../assets/products/thumbnail/3.png"),
+                ingredients: [
+                    "Pão brioche",
+                    "110g de carne grelhada",
+                    "Queijo derretido",
+                    "Banco crocante",
+                    "Alface",
+                    "Tomate",
+                    "Cebola caramelizada",
+                    "Molho barbecue",
+                ],
+            },
+            {
+                id: "4",
+                title: "Hambúrguer Mediterrâneo",
+                price: 29.9,
+                description:
+                    "Uma opção diferente com carne de cordeiro suculenta, queijo feta cremoso, tomate seco, rúcula fresca e azeite de oliva, servido em um pão de hambúrguer tradicional.",
+                cover: require("../../assets/products/cover/4.png"),
+                thumbnail: require("../../assets/products/thumbnail/4.png"),
+                ingredients: [
+                    "Pão brioche",
+                    "Carne de cordeiro (140g)",
+                    "Queijo feta cremoso",
+                    "Rúcula fresca e Azeite",
+                    "Tomate seco",
+                    "Picles",
+                    "Molho da casa",
+                ],
+            },
         ],
-      },
-      {
-        id: "3",
-        title: "X-Cobol",
-        price: 32.7,
-        description:
-          "Aquele hambúrguer que não podemos tirar do cardápio devido aos clientes mais antigos da hamburgueria...",
-        cover: require("../../assets/products/cover/3.png"),
-        thumbnail: require("../../assets/products/thumbnail/3.png"),
-        ingredients: [
-          "Pão brioche;",
-          "2x carnes smash (blend da casa) de 80g;",
-          "Queijo cheddar;",
-          "Alface;",
-          "Tomate;",
-          "Picles;",
-          "Cebola;",
-          "Molho da casa;",
+    },
+    {
+        title: "Sobremesa",
+        data: [
+            {
+                id: "5",
+                title: "Sundae Supreme",
+                price: 18.9,
+                description:
+                    "Uma bola de sorvete de creme cremoso, coberta com calda de chocolate quente, chantilly farto, pedaços crocantes de waffle e cerejas suculentas.",
+                cover: require("../../assets/products/cover/5.png"),
+                thumbnail: require("../../assets/products/thumbnail/5.png"),
+                ingredients: [
+                    "Brownie",
+                    "1x Bola de sorvete de creme",
+                    "Calda de chocolate",
+                    "Chantilly",
+                    "Pedaços crocantes de waffle",
+                ],
+            },
+            {
+                id: "6",
+                title: "Cupcake Delícia",
+                price: 22.9,
+                description:
+                    "Um deleite para os amantes de cupcakes, recheado com sorvete de baunilha cremoso, coberto com uma montanha de chantilly, granulado colorido e uma cereja no topo para finalizar.",
+                cover: require("../../assets/products/cover/6.png"),
+                thumbnail: require("../../assets/products/thumbnail/6.png"),
+                ingredients: [
+                    "Sorvete de baunilha",
+                    "Chantilly",
+                    "Granulado colorido",
+                    "Cereja",
+                ],
+            },
         ],
-      },
-      {
-        id: "4",
-        title: "X-Tailwind",
-        price: 29.9,
-        description:
-          "Aquele que você não gosta de primeira, mas depois fica viciado e não consegue mais largar...",
-        cover: require("../../assets/products/cover/4.png"),
-        thumbnail: require("../../assets/products/thumbnail/4.png"),
-        ingredients: [
-          "Pão brioche;",
-          "2x carnes smash (blend da casa) de 80g;",
-          "Queijo cheddar;",
-          "Alface;",
-          "Tomate;",
-          "Picles;",
-          "Cebola;",
-          "Molho da casa;",
+    },
+    {
+        title: "Bebidas",
+        data: [
+            {
+                id: "7",
+                title: "Coca Cola 350ml",
+                price: 6.9,
+                thumbnail: require("../../assets/products/thumbnail/7.png"),
+                cover: require("../../assets/products/cover/7.png"),
+                description:
+                    "Uma coca super gelada para acompanhar o seu super lanche e refrescar o seu dia.",
+                ingredients: [],
+            },
         ],
-      },
-    ],
-  },
-  {
-    title: "Sobremesa",
-    data: [
-      {
-        id: "5",
-        title: "Sorvete com Brownie",
-        price: 18.9,
-        description:
-          "Uma sobremesa deliciosa para saborear. Escolha o sorvete e a calda que desejar...",
-        cover: require("../../assets/products/cover/5.png"),
-        thumbnail: require("../../assets/products/thumbnail/5.png"),
-        ingredients: [
-          "1x Brownie;",
-          "1x Bola de sorvete a sua escolha",
-          "Escolha sua calda;",
-        ],
-      },
-      {
-        id: "6",
-        title: "Cupcake",
-        price: 22.9,
-        description:
-          "Um delicioso Cupcake para adoçar. Escolha o sabor que você gosta...",
-        cover: require("../../assets/products/cover/6.png"),
-        thumbnail: require("../../assets/products/thumbnail/6.png"),
-        ingredients: ["Escolha o sabor;", "Chantilly;"],
-      },
-    ],
-  },
-  {
-    title: "Bebidas",
-    data: [
-      {
-        id: "7",
-        title: "Hmmm, coquinha!",
-        price: 6.9,
-        thumbnail: require("../../assets/products/thumbnail/7.png"),
-        cover: require("../../assets/products/cover/7.png"),
-        description:
-          "Uma coca super gelada para acompanhar o seu super lanche...",
-        ingredients: [],
-      },
-    ],
-  },
-]
+    },
+];
 
-const PRODUCTS = MENU.map((item) => item.data).flat()
+const PRODUCTS = MENU.map((item) => item.data).flat();
 
-const CATEGORIES = MENU.map((item) => item.title)
+const CATEGORIES = MENU.map((item) => item.title);
 
-type ProductProps = (typeof PRODUCTS)[0]
+type ProductProps = (typeof PRODUCTS)[0];
 
-export { MENU, PRODUCTS, CATEGORIES, ProductProps }
+export { CATEGORIES, MENU, PRODUCTS, ProductProps };
