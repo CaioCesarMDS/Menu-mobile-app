9c2489f87adf77a607b0dd7ea398309ea3c217c6
CaioCesarMDS <caiocesarsts@gmail.com>
2024-02-11 15:18:44 -0300

    feat: add product details screen

---
+++ src/app/product/[id].tsx
@@ -0,0 +1,50 @@
+import { AddCartButton } from "@/components/add-cart-button";
+import { PRODUCTS } from "@/utils/data/products";
+import { formatCurrency } from "@/utils/functions/format-currency";
+import { Feather } from "@expo/vector-icons";
+import { useLocalSearchParams } from "expo-router";
+import { Image, Text, View } from "react-native";
+
+const Product = () => {
+    const { id } = useLocalSearchParams();
+
+    const product = PRODUCTS.filter((item) => item.id === id)[0];
+
+    return (
+        <View className="flex-1">
+            <Image source={product.cover} className="w-full h-52 " resizeMode="cover" />
+            <View className="p-5 mt-4 flex-1">
+                <Text className="text-lime-400 text-2xl font-heading">
+                    {product.title}
+                </Text>
+                <Text className="text-slate-400 text-base leading-6 font-body mt-3">
+                    {product.description}
+                </Text>
+                <Text className="text-lime-400  text-2xl font-heading mt-6">
+                    {formatCurrency(product.price)}
+                </Text>
+                <Text className="text-slate-100 text-xl  font-subtitle mt-6 mb-3">
+                    Ingredientes:
+                </Text>
+                {product.ingredients.map((ingredient) => (
+                    <Text
+                        key={ingredient}
+                        className="text-slate-400 text-base font-body  leading-6 "
+                    >
+                        {`\u2022 ${ingredient}`}
+                    </Text>
+                ))}
+            </View>
+            <View className="p-5 pb-8 gap-5">
+                <AddCartButton>
+                    <AddCartButton.Icon>
+                        <Feather name="plus-circle" size={16} color="#000" />
+                    </AddCartButton.Icon>
+                    <AddCartButton.Text>Adicionar ao pedido</AddCartButton.Text>
+                </AddCartButton>
+            </View>
+        </View>
+    );
+};
+
+export default Product;
--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,4 +1,12 @@
-import { Image, ImageProps, Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
+import { forwardRef } from "react";
+import {
+    Image,
+    ImageProps,
+    Text,
+    TouchableOpacity,
+    TouchableOpacityProps,
+    View,
+} from "react-native";
 
 type ProductDataProps = {
     title: string;
@@ -10,17 +18,24 @@ type ProductProps = TouchableOpacityProps & {
     data: ProductDataProps;
 };
 
-export const Product = ({ data, ...rest }: ProductProps) => {
-    return (
-        <TouchableOpacity
-            className="w-full flex-row items-center pb-4"
-            {...rest}
-        >
-            <Image source={data.thumbnail} className="w-20 h-20 rounded-md"/>
-            <View className="flex-1 ml-3">
-                <Text className="flex-1 text-slate-100 font-subtitle text-base ">{data.title}</Text>
-                <Text className="text-slate-400 text-xs leading-5 mt-0.5">{data.description}</Text>
-            </View>
-        </TouchableOpacity>
-    );
-};
+export const Product = forwardRef<TouchableOpacity, ProductProps>(
+    ({ data, ...rest }, ref) => {
+        return (
+            <TouchableOpacity
+                ref={ref}
+                className="w-full flex-row items-center pb-4"
+                {...rest}
+            >
+                <Image source={data.thumbnail} className="w-20 h-20 rounded-md" />
+                <View className="flex-1 ml-3">
+                    <Text className="flex-1 text-slate-100 font-subtitle text-base ">
+                        {data.title}
+                    </Text>
+                    <Text className="text-slate-400 text-xs leading-5 mt-0.5">
+                        {data.description}
+                    </Text>
+                </View>
+            </TouchableOpacity>
+        );
+    }
+);
